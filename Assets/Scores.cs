3b14ae9e190bd9ab4c5246dfbd57c70055468dc5
Jashan Goyal <jashangoyal98@gmail.com>
2019-06-13 11:39:14 +0530

    connected with firebase

---
+++ Assets/Scores.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Proyecto26;
+public class Scores : MonoBehaviour
+{
+    public Text scoreText;
+    public InputField nameText;
+
+    private System.Random random = new System.Random();
+
+    public static int playerScore;
+    public static string playerName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScore = random.Next(0, 101);
+       // scoreText.text = "Score: " + playerScore;
+    }
+
+    public void OnSubmit()
+    {
+        playerName = nameText.text;
+        PostToDatabase();
+    }
+
+    public void OnGetScore()
+    {
+        playerName = nameText.text;
+        RetriveFromDatabase();
+    }
+
+    private void PostToDatabase()
+    {
+        User user = new User();
+        RestClient.Put("https://scalageek-c05ef.firebaseio.com/"+ playerName +".json", user);
+    }
+
+    private void RetriveFromDatabase()
+    {
+        RestClient.Get<User>("https://scalageek-c05ef.firebaseio.com/" + playerName + ".json").Then(response =>
+          {
+              scoreText.text = response.userName;
+              return response;
+          });
+    }
+
+
+}
---
+++ Assets/vuforia.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vuforia : MonoBehaviour, ITrackableEventHandler
+{
+    TrackableBehaviour new_TrackableBehaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        new_TrackableBehaviour = GetComponent<TrackableBehaviour>();
+        VuMarkManager new_Vumark;
+        if(new_TrackableBehaviour!=null)
+        {
+            new_TrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+        new_Vumark = TrackerManager.Instance.GetStateManager().GetVuMarkManager();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if(newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackerFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED && newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            OnTrackerLost();
+        }
+    }
+
+    void OnTrackerFound()
+    {
+         
+    }
+
+    void OnTrackerLost()
+    {
+
+    }
+
+}
